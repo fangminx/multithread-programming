03fba6f3511cec48f1de0f84fc9573430d8d125b
fangmin <min.fang@huilianyi.com>
2018-02-19 22:14:48 +0800

    使用原子类，进行原子性操作

--- src/main/java/com/fangminx/multithreadprogramming/volatileAndSynchronized/VolatileDemo.java
+++ src/main/java/com/fangminx/multithreadprogramming/volatileAndSynchronized/VolatileDemo.java
@@ -1,17 +1,35 @@
 package com.fangminx.multithreadprogramming.volatileAndSynchronized;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 public class VolatileDemo {
 
     public volatile int a = 1;
 
+    //使用原子类
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private int [] s = {2,1,4,6};
+    AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(s);
+
     /**
-     * volatile不能保证非原子性操作，
+     * volatile不能保证非原子性操作，a++为非原子性操作
      * synchronized可以取代volatile，volatile不可以取代synchronized
      * @return
      */
     public synchronized int getA(){
         return a++;
     }
+
+    /**
+     * 使用原子类保证原子性操作
+     * @return
+     */
+    public int getValueNext(){
+        return atomicInteger.getAndIncrement(); //底层使用cas(compareAndSet)操作
+    }
+
 }
 
 class VolatileDemoMain {
